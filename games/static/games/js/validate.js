e707c5f2c9a3b37f684713b6658be6694a843bb7
SgtFreddy713 <captesxel@gmail.com>
2026-01-18 19:30:38 -0300

    Add expected_answer in session. Add id to num_1 and num_2 in template. Update "validate" view.  Create updateMathGame function in js.

--- games/static/games/js/validate.js
+++ games/static/games/js/validate.js
@@ -1,6 +1,24 @@
+function updateMathGame(data) {
+    let num_1 = document.getElementById("num-1")
+    let num_2 = document.getElementById("num-2")
+
+    num_1.textContent = data["num_1"]
+    num_2.textContent = data["num_2"]
+    document.getElementById("math-answer").value = ""
+
+
+
+    console.log(data)
+
+}
+
 function handleMathGame() {
     const answer = document.getElementById("math-answer").value
 
+    if (answer == "") {
+        return
+    }
+        
     fetch("/games/api/validate/", {
         method: "POST",
         body: JSON.stringify({ "answer": answer}),
@@ -9,8 +27,8 @@ function handleMathGame() {
         },
     })
       .then((res) => res.json())
-      .then((res) => console.log(res))
+      .then((res) => updateMathGame(res))
     
 }
 
-document.getElementById("submit-math-answer").addEventListener("click", function(){handleMathGame()})
+document.getElementById("submit-math-answer").addEventListener("click", handleMathGame)
--- games/views.py
+++ games/views.py
@@ -7,14 +7,33 @@ from . import serializers
 # Create your views here.
 
 def index(request):
+    grid = game_engine.generate_grid_game()
+    math_problem = game_engine.generate_math_game()
+
+    request.session["expected_answer"] = math_problem["answer"]
+
     context = {
-        'grid': game_engine.generate_grid_game(),
-        'math_problem': game_engine.generate_math_game()
+        'grid': grid,
+        'math_problem': math_problem
     }
+    
     return render(request, 'games/index.html', context)
 
 @api_view(['POST'])
 def validate(request):
     serializer = serializers.mathSerializer(data=request.data)
     serializer.is_valid(raise_exception=True)
-    return Response(serializer.data)
+
+    user_answer = serializer.validated_data["answer"]
+    expected_answer = request.session["expected_answer"]
+
+    correct = user_answer == expected_answer
+
+    new_math_problem = game_engine.generate_math_game()
+    request.session["expected_answer"] = new_math_problem["answer"]
+
+    return Response({
+        "correct": correct,
+        "num_1": new_math_problem["num_1"],
+        "num_2": new_math_problem["num_2"]
+    })
