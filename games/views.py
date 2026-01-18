02f18de2566b68097665edf85ce9522f8c2ea0f8
SgtFreddy713 <captesxel@gmail.com>
2026-01-18 14:22:02 -0300

    add math-game div to template and pass math_problem in context

--- games/views.py
+++ games/views.py
@@ -5,6 +5,7 @@ from . import game_engine
 
 def index(request):
     context = {
-        'grid': game_engine.generate_grid_game()
+        'grid': game_engine.generate_grid_game(),
+        'math_problem': game_engine.generate_math_game()
     }
     return render(request, 'games/index.html', context)
