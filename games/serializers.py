c63f8c76e35101b086ef39c6cfdd0f4e1b7fe6bc
SgtFreddy713 <captesxel@gmail.com>
2026-01-19 19:43:42 -0300

    Add gridSerializer

--- games/serializers.py
+++ games/serializers.py
@@ -2,3 +2,6 @@ from rest_framework import serializers
 
 class mathSerializer(serializers.Serializer):
     answer = serializers.IntegerField()
+
+class gridSerializer(serializers.Serializer):
+    answer = serializers.ChoiceField(choices=["red", "blue"])
