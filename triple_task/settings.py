07b15ef8cd29797b4d5cc3800a773a3c493dd693
SgtFreddy713 <captesxel@gmail.com>
2026-02-10 00:01:42 -0300

    Add CustomUser model and configure AUTH_USER_MODEL

--- triple_task/settings.py
+++ triple_task/settings.py
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
     'rest_framework'
 ]
 
+AUTH_USER_MODEL = 'accounts.CustomUser'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
